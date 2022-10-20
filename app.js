13e3e4aae59d364258130947f3511f6f31eb6219
Varun Bogulla <varunub1505@gmail.com>
2022-10-20 20:03:25 +0530

    Level1

---
+++ app.js
@@ -0,0 +1,85 @@
+const express=require("express")
+const bp=require("body-parser")
+const ejs=require("ejs")
+const mongoose=require("mongoose")
+const encrypt=require("mongoose-encryption")
+mongoose.connect("mongodb://localhost:27017/userDB")
+const mySchema=new mongoose.Schema({
+    Username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+const secret="Varunisawasteboy"
+mySchema.plugin(encrypt,{secret:secret,encryptedFields: ['password']})
+const user=mongoose.model("user",mySchema)
+
+const app=express()
+
+app.use(bp.urlencoded({
+    extended:true
+}))
+app.use(express.static("public"))
+app.set('view engine','ejs')
+
+app.get("/",function(req,res){
+    res.render("home")
+})
+app.get("/register",function(req,res){
+    res.render("register")
+})
+
+app.post("/register",function(req,res){
+    const newUser=new user({
+        Username:req.body.username,
+        password:req.body.password
+    })
+    newUser.save(function(err){
+        if(err){
+            console.log(err)
+        }else{
+            res.render("secrets")
+        }
+    })
+    
+})
+
+app.get("/login",function(req,res){
+    res.render("login")
+})
+app.post("/login",function(req,res){
+    //////////////////////One way/////////////////
+    // user.findOne({$and:[{Username:req.body.username},{password:req.body.password}]},function(err,result){
+    //     if(result){
+    //         res.render("secrets")
+    //     }
+    //     else{
+    //         res.send("Register First")
+    //     }
+    // })
+
+    /////////////////Second Way////////////////////
+
+    user.findOne({Username:req.body.username},function(err,result){
+        // console.log(result.password)
+        if(result){
+            if(result.password === req.body.password){
+                res.render("secrets")
+            }
+            else{
+                res.send("You Entered Wrong Password")
+            }
+        }
+        else{
+            res.send("Register First")
+        }
+    })
+})
+
+app.listen(3000,function(){
+    console.log("Server started at port 3000")
+})
